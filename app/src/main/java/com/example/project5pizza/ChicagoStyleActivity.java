c7c16124bed0b600b16b816cdff34c210e310e84
AryaShetty08 <arya.shetty08@gmail.com>
2022-11-29 18:34:25 -0500

    add stuff to project 5

---
+++ app/src/main/java/com/example/project5pizza/ChicagoStyleActivity.java
@@ -0,0 +1,15 @@
+package com.example.project5pizza;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChicagoStyleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chicagostylepizza);
+    }
+
+}
