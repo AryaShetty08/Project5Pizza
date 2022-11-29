97d70249fa182436be306fcec8e0139fd06ba1b4
JohnFGC <jfgc2002@gmail.com>
2022-11-29 18:22:43 -0500

    Hello

--- app/src/main/java/com/example/project5pizza/MainActivity.java
+++ app/src/main/java/com/example/project5pizza/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //hello
+
         //ImageView chicagoStyle = (ImageView)findViewById(R.id.chicagoStyle);
         //chicagoStyle.setBackgroundResource(R.drawable.chicagopizzaimage);
     }
