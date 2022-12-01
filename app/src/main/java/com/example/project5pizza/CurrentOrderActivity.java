70b12b6bb5c8275d5802f5840285fe700c99094b
AryaShetty08 <arya.shetty08@gmail.com>
2022-11-30 20:23:04 -0500

    add stuff to project 5

---
+++ app/src/main/java/com/example/project5pizza/CurrentOrderActivity.java
@@ -0,0 +1,17 @@
+package com.example.project5pizza;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CurrentOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.currentorder);
+
+    }
+
+}
